927e49a6329de05cd13a50cf355094760825d1ef
chaudhga <gaurav.chaudhari@gmail.com>
2022-01-18 18:36:20 +0800

    Updated error message if web3 not available

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Make sure Web3 provider like metamask is installed and rety!`,
       );
       console.error(error);
     }
